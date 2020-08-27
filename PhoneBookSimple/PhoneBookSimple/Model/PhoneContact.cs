2a88278d7790f94ae258a9ebe425fec9d4d82017
Dave Hickling <dave.hickling@live.co.uk>
2020-08-27 15:20:46 +0100

    Initial commit.

---
+++ PhoneBookSimple/PhoneBookSimple/Model/PhoneContact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PhoneBookSimple.Model
+{
+	public class PhoneContact
+	{
+		public string name { get; set; }
+		public string phone_number { get; set; }
+		public string address { get; set; }
+	}
+
+	public class PhoneRoot
+	{
+		public List<PhoneContact> contacts { get; set; }
+	}
+}
